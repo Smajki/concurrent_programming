cc0b8554c917e4181e7505489c050ea42f71967c
Jakub Szewczyk <254878@edu.p.lodz.pl>
2026-04-15 00:09:10 +0200

    chyba jeszcze nie działa

--- ProgramowanieWspolbiezne/View/MainWindow.xaml.cs
+++ ProgramowanieWspolbiezne/View/MainWindow.xaml.cs
@@ -1,34 +1,38 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using Data;
+using Logic;
+using ModelView;
 
 namespace View
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DispatcherTimer _timer;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+            IBallRepository repository = new BallRepository();
+            ISimulationLogic logic = new SimulationLogic(repository);
+            var vm = new MainViewModel(logic);
 
-        }
+            DataContext = vm;
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(16)
+            };
 
+            _timer.Tick += (_, _) =>
+            {
+                if (DataContext is MainViewModel vm)
+                    vm.Tick();
+            };
+
+            _timer.Start();
         }
     }
 }
