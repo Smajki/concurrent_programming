d1d3d8d682d1b4c22b2526adc8457539e0b81a5f
Agnieszka Borowiec <254722@edu.p.lodz.pl>
2026-04-18 17:42:09 +0200

    dodanie kolejnych testów

---
+++ ProgramowanieWspolbiezne/LogicTest/Test2.cs
@@ -0,0 +1,41 @@
+﻿using Data;
+using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTest
+{
+    [TestClass]
+    public sealed class Test2
+    {
+        [TestMethod]
+        public void UpdateFromSetsCorrectValues()
+        {
+            var b = new BallExtended();
+            var ball = new Ball(100, 200, 0, 0, 50);
+            b.UpdateFrom(ball);
+            Assert.AreEqual(100 - 25, b.X);
+            Assert.AreEqual(200 - 25, b.Y);
+            Assert.AreEqual(50, b.Diameter);
+        }
+        [TestMethod]
+        public void UpdateFromRaisesPropertyChangedForAllPropertiesAndDoesNotRaisePropertyChangedWhenValuesAreSame()
+        {
+            var vm = new BallExtended();
+            var ball = new Ball(10, 20, 0, 0, 10);
+
+            var changed = new List<string>();
+            vm.PropertyChanged += (_, e) => changed.Add(e.PropertyName!);
+            vm.UpdateFrom(ball);
+            CollectionAssert.Contains(changed, "X");
+            CollectionAssert.Contains(changed, "Y");
+            CollectionAssert.Contains(changed, "Diameter");
+            changed.Clear();
+            vm.UpdateFrom(ball); // te same wartości
+            Assert.AreEqual(0, changed.Count);
+        }
+    }
+}
