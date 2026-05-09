1a0b60ecf8e90a34eeb061d928eb03786105e4ee
Smajki <smajki-5@wp.pl>
2026-05-09 02:46:12 +0200

    poprawka

--- ProgramowanieWspolbiezne/Data/Ball.cs
+++ ProgramowanieWspolbiezne/Data/Ball.cs
@@ -6,9 +6,9 @@
 
         public Vector Velocity { get; set; }
 
-        public double Diameter { get;}
+        public double Diameter { get; }
 
-        public double Mass { get;}
+        public double Mass { get; }
 
         public double Id { get; set; }
 
@@ -19,23 +19,21 @@
             this.Diameter = diameter;
             this.Mass = mass;
         }
-        public Ball(double positionX, double positionY, double velocityX, double velocityY, double diameter) {
-            Position = new Vector(positionX, positionY);
-            Velocity = new Vector(velocityX, velocityY);
-            this.Diameter = diameter;
-            this.Mass = 1; //dla uproszczenia - jeśli nie podano masy, masa jest równa 1 kg
-        }
 
-        public void Collide(IBall other) {
-            double otherBallMass=other.Mass;
+        public Ball(double positionX, double positionY, double velocityX, double velocityY, double diameter)
+        : this(positionX, positionY, velocityX, velocityY, diameter, 1)//dla uproszczenia - jeśli nie podano masy, masa jest równa 1 kg
+        { }
+        public void Collide(IBall other)
+        {
+            double otherBallMass = other.Mass;
             Vector newVelocity;
             Vector newOtherBallVelocity;
-            if (Mass == otherBallMass) 
+            if (Mass == otherBallMass)
             {
                 newVelocity = other.Velocity;
                 newOtherBallVelocity = Velocity;
             }
-            else 
+            else
             {
                 double sumOfMasses = Mass + otherBallMass;
                 newVelocity = Velocity.MultiplyByNumber((Mass - otherBallMass) / sumOfMasses)
